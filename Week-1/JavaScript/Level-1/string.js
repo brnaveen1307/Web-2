6e6eaac9ce8e359cfcb175016dbc1c2856788785
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-08-07 22:51:09 +0530

    String functions

---
+++ Week-1/JavaScript/Level-1/string.js
@@ -0,0 +1,54 @@
+//Length
+
+function getLength(str)
+{
+    console.log("Original String: ", str);
+    console.log("Length of String: ", str.length);
+}   
+
+getLength("Hello World!"); 
+
+
+//indexOf
+function findIndexOf(str, target)
+{
+    console.log("Original String: ", str);
+    console.log("Index of '" + target + "': ", str.indexOf(target));
+}
+
+findIndexOf("Hello World!", "World");
+
+// lastIndexOf
+function findLastIndexOf(str, target) {
+  console.log("Original String:", str);
+  console.log("Index:", str.lastIndexOf(target));
+}
+findLastIndexOf("Hello World World", "World");
+
+// slice
+function getSlice(str, start, end) {
+  console.log("Original String:", str);
+  console.log("After slice:", str.slice(start, end));
+}
+getSlice("Hello World", 0, 5);
+
+// substring
+function getSubstring(str, start, end) {
+  console.log("Original String:", str);
+  console.log("After substring:", str.substring(start, end));
+}
+getSubstring("Hello World", 0, 5);
+
+// replace
+function replaceString(str, target, replacement) {
+  console.log("Original String:", str);
+  console.log("After replace:", str.replace(target, replacement));
+}
+replaceString("Hello World", "World", "JavaScript");
+
+// split
+function splitString(str, separator) {
+  console.log("Original String:", str);
+  console.log("After split:", str.split(separator));
+}
+splitString("Hello World", " ");
