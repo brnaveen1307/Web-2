05aa13ee8d61ada76a638037fff071d24493650b
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-08-07 22:59:33 +0530

    Added toUpperCase function

--- Week-1/JavaScript/Level-1/string.js
+++ Week-1/JavaScript/Level-1/string.js
@@ -19,36 +19,50 @@ function findIndexOf(str, target)
 findIndexOf("Hello World!", "World");
 
 // lastIndexOf
-function findLastIndexOf(str, target) {
+function findLastIndexOf(str, target) 
+{
   console.log("Original String:", str);
   console.log("Index:", str.lastIndexOf(target));
 }
 findLastIndexOf("Hello World World", "World");
 
 // slice
-function getSlice(str, start, end) {
+function getSlice(str, start, end) 
+{
   console.log("Original String:", str);
   console.log("After slice:", str.slice(start, end));
 }
 getSlice("Hello World", 0, 5);
 
 // substring
-function getSubstring(str, start, end) {
+function getSubstring(str, start, end)
+{
   console.log("Original String:", str);
   console.log("After substring:", str.substring(start, end));
 }
 getSubstring("Hello World", 0, 5);
 
 // replace
-function replaceString(str, target, replacement) {
+function replaceString(str, target, replacement) 
+{
   console.log("Original String:", str);
   console.log("After replace:", str.replace(target, replacement));
 }
 replaceString("Hello World", "World", "JavaScript");
 
 // split
-function splitString(str, separator) {
+function splitString(str, separator)
+{
   console.log("Original String:", str);
   console.log("After split:", str.split(separator));
 }
 splitString("Hello World", " ");
+
+//toUpperCase
+function toUpperCase(str) 
+{
+  console.log("Original String:", str);
+  console.log("After toUpperCase:", str.toUpperCase());
+}
+
+console.log(toUpperCase("Hello World"));
