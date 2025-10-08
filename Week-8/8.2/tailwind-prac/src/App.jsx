5c7cd33b08611dcb688f6483a5a6a10ba2c41e2c
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-10-08 23:22:15 +0530

    Added RevenueCard

--- Week-8/8.2/tailwind-prac/src/App.jsx
+++ Week-8/8.2/tailwind-prac/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {RevenueCard} from './components/RevenueCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="">
-
+      <div className="grid grid-cols-4">
+      <RevenueCard title={"Amount Pending"} amount={"92,321.20"} orderCount={13} />
       </div>
     </>
   )
--- Week-8/8.2/tailwind-prac/src/components/RevenueCard.jsx
+++ Week-8/8.2/tailwind-prac/src/components/RevenueCard.jsx
@@ -1,6 +1,22 @@
 
-export function RevenueCard(){
-    return <div className="bg-white">
+export function RevenueCard({
+    title,
+    orderCount,
+    amount
+}){
+    return <div className="bg-white rounded shadow-md p-4">
+        <div className="text-gray">
+            {title}
+            ?
+        </div>
 
-    </div>
+        <div className="flex justify-between">
+            <div>
+                ₹ {amount}
+            </div>
+            {orderCount ? <div>
+                {orderCount} order(s) {">"}
+            </div> : null}
+        </div>
+    </div>    
 }
