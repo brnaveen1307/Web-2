7f37abea050c0d9306cb2d35090ab519cde7f739
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-10-12 22:30:08 +0530

    Started with TypeScript

---
+++ Week-9/9.2/node-app/a.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 1;
+console.log(x);
+function f(firstName) {
+    console.log("Hello " + firstName);
+}
+f("Naveen");
+//# sourceMappingURL=a.js.map
---
+++ Week-9/9.2/node-app/a.ts
@@ -0,0 +1,6 @@
+
+function f(firstName: string){
+    console.log("Hello " + firstName)
+}
+
+f("Naveen")
