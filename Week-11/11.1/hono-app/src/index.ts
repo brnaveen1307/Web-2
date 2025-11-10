365c6e71c291bfb5c625ec06c37793e5091dee8c
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-11-10 21:56:39 +0530

    Started with AWS Deployment

--- Week-11/11.1/hono-app/src/index.ts
+++ Week-11/11.1/hono-app/src/index.ts
@@ -4,7 +4,18 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/', async (c) => {
+async function authMiddleware(c: any, next: any){
+  if(c.req.header("Authorization")){
+    const initTime = new Date()
+    await next()
+    const totalTime = (new Date().getTime() - initTime.getTime()) / 1000
+    console.log(`It took ${totalTime} seconds`)
+  }else{
+    return c.text("You don't have access")
+  }
+}
+
+app.post('/', authMiddleware, async (c) => {
   const  body = await c.req.json()
   console.log(body)
   console.log(c.req.header("Authorization"))
