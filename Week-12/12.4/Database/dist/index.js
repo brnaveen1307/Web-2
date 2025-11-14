4ca434d9f27adc7a7c64ed2c6532f587a6cd77f8
brnaveen1307 <brnaveenkumar1307@gmail.com>
2025-11-14 13:33:00 +0530

    PSQL

---
+++ Week-12/12.4/Database/dist/index.js
@@ -0,0 +1,19 @@
+import { Client } from 'pg';
+const client = new Client({
+    connectionString: "postgresql://neondb_owner:npg_m83ouizMCSxe@ep-fragrant-sky-a475uu6q-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+});
+async function createUsersTable() {
+    await client.connect();
+    const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEy,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );    
+    `);
+    console.log(result);
+}
+createUsersTable();
+//# sourceMappingURL=index.js.map
